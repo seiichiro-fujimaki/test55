b229a77ffdd92296121d2a0e11613b8bd74d950a
誠一郎 <sinada@trust-coms.com>
2024-12-05 19:02:47 +0900

    3kai

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,14 +1,24 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { helloWorld } from './function/hello-world/resource'
+import { invokeBedrock } from './function/invoke-bedrock/resource'
+import * as iam from 'aws-cdk-lib/aws-iam';
 const backend = defineBackend({
         auth,
         helloWorld,
+        invokeBedrock,
 });
 const authenticatedUserIamRole = backend.auth.resources.authenticatedUserIamRole;
 backend.helloWorld.resources.lambda.grantInvoke(authenticatedUserIamRole);
 backend.addOutput({
         custom: {
         helloWorldFunctionName: backend.helloWorld.resources.lambda.functionName,
+        invokeBedrockFunctionName: backend.invokeBedrock.resources.lambda.functionName,
         },
 });
+backend.invokeBedrock.resources.lambda.grantInvoke(authenticatedUserIamRole);
+const bedrockStatement = new iam.PolicyStatement({
+                actions: ["bedrock:InvokeModel", "bedrock:InvokeModelWithResponseStream"],
+                resources: ["arn:aws:bedrock:us-east-1::foundation-model/*"]
+        })
+backend.invokeBedrock.resources.lambda.addToRolePolicy(bedrockStatement)
